4dd525e69d62be7a3ea1e0aa2f19dc887c00f7e7
Mhamad Belal Al Msalma <117995558+MhamadBelal@users.noreply.github.com>
2023-07-01 01:01:11 +0300

    Lab04 first commit

---
+++ Lab04_TicTacToeProgram/Classes/Position.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04_TicTacToeProgram.Classes
+{
+    class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        /// <summary>
+        /// Position on the gameboard being initialized 
+        /// </summary>
+        /// <param name="row">row number</param>
+        /// <param name="column">column number</param>
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+
+    
+}
---
+++ Lab04_TicTacToeTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Lab04_TicTacToeTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
